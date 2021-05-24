2cac3662537edb9a916761c2628e2fa12c8efc51
Jared Peter <aljapeter05@gmail.com>
2021-05-24 06:13:48 +0600

    Updated serviceWorker.js

---
+++ public/serviceWorker.js
@@ -0,0 +1,20 @@
+let CACHE_NAME = 'todo';
+const urlsToCache = ['/todo/', '/todo/index.html'];
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(response => {
+        if (response) {
+            return response;
+        }
+        return fetch(e.request);
+    }));
+    self.skipWaiting();
+});
